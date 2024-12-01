3cca7d58eee607b36b6f650d12e99e49dcbcd9c4
Hemanth <hemanthsunilkumar28@gmail.com>
2024-12-01 21:46:52 +0530

    update

--- __main__.py
+++ __main__.py
@@ -8,22 +8,22 @@ from src.generate_traffic import generate_random_numbers_in_file
 def main():
     gpio_setup()
     # -------------------------- COMMENT IF MASTER -----------------------------
-    # start_req_handler_thread = threading.Thread(target=start_req_handler)
-    # start_req_handler_thread.start()
+    start_req_handler_thread = threading.Thread(target=start_req_handler)
+    start_req_handler_thread.start()
 
 
     # --------------------------------------------------------------------------
 
 
     # ---------- COMMENT IF NOT MASTER---------------------------
-    sync_req_master_thread = threading.Thread(target=reliable_sync_request_master)
-    sync_ack_master_thread = threading.Thread(target=reliable_sync_ack_master)
-    start_req_master_thread = threading.Thread(target=reliable_start)
-    start_ack_master_thread = threading.Thread(target=reliable_start_ack)
-    sync_req_master_thread.start()
-    sync_ack_master_thread.start()
-    start_req_master_thread.start()
-    start_ack_master_thread.start()
+    #sync_req_master_thread = threading.Thread(target=reliable_sync_request_master)
+    #sync_ack_master_thread = threading.Thread(target=reliable_sync_ack_master)
+    #start_req_master_thread = threading.Thread(target=reliable_start)
+    #start_ack_master_thread = threading.Thread(target=reliable_start_ack)
+    #sync_req_master_thread.start()
+    #sync_ack_master_thread.start()
+    #start_req_master_thread.start()
+    #start_ack_master_thread.start()
     # ---------------------------------------------------------------------------
 
 
--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 
 MASTER_CONTROLLER_ID = 1
-CONTROLLER_ID = 1
+CONTROLLER_ID = 2
 DEVICES = 4
 
 MULTICAST_GROUP = "224.0.0.1"
@@ -10,9 +10,9 @@ FILE_PATH = f'queue1.txt'
 
 TIME_OFFSET = 60
 SLOT_TIME = 10
-RTO = 10
+RTO = 30
 RETRIES = 0
-MAX_RETRIES = 20
+MAX_RETRIES = 7
 
 SYNC_REQ_MESSAGE = {"type": "sync_request", "controller_id": MASTER_CONTROLLER_ID}
 SYNC_ACK_MESSAGE = {"type": "sync_ack", "controller_id": CONTROLLER_ID}
