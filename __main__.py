4eac741f0832f5bc3744d16772035a703f090333
Kartik <animeking3121997@gmail.com>
2024-12-03 16:47:48 +0530

    update

--- __main__.py
+++ __main__.py
@@ -13,20 +13,20 @@ def main():
     # START_SUCCESS.set()
 
 
-    if(CONTROLLER_ID==MASTER_CONTROLLER_ID):
-        sync_req_master_thread = threading.Thread(target=reliable_sync_request_master)
-        sync_ack_master_thread = threading.Thread(target=reliable_sync_ack_master)
-        start_req_master_thread = threading.Thread(target=reliable_start)
-        start_ack_master_thread = threading.Thread(target=reliable_start_ack)
-        sync_req_master_thread.start()
-        sync_ack_master_thread.start()
-        start_req_master_thread.start()
-        start_ack_master_thread.start()
-    else:
-        handle_sync_requests_thread = threading.Thread(target=handle_sync_requests)
-        start_req_handler_thread = threading.Thread(target=start_req_handler)
-        handle_sync_requests_thread.start()
-        start_req_handler_thread.start()
+    # if(CONTROLLER_ID==MASTER_CONTROLLER_ID):
+    sync_req_master_thread = threading.Thread(target=reliable_sync_request_master)
+    sync_ack_master_thread = threading.Thread(target=reliable_sync_ack_master)
+    start_req_master_thread = threading.Thread(target=reliable_start)
+    start_ack_master_thread = threading.Thread(target=reliable_start_ack)
+    sync_req_master_thread.start()
+    sync_ack_master_thread.start()
+    start_req_master_thread.start()
+    start_ack_master_thread.start()
+    # else:
+    handle_sync_requests_thread = threading.Thread(target=handle_sync_requests)
+    start_req_handler_thread = threading.Thread(target=start_req_handler)
+    handle_sync_requests_thread.start()
+    start_req_handler_thread.start()
 
 
 
